007d9c3222713ccc34ff52cc3bcda73b8bdf87af
danielebd <danielebalebalestro@gmail.com>
2023-03-15 15:36:51 +0100

    stampare con v-for l'array

--- js/main.js
+++ js/main.js
@@ -4,7 +4,26 @@ const {createApp} = Vue;
 
 createApp({
     data() {
-        return
+        return {
+            toDo: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'andare dal medico',
+                    done: false
+                },
+                {
+                    text: 'chimare il meccanico peer cambio gomme',
+                    done: false
+                },
+                {
+                    text: 'chiamare ufficio personale',
+                    done: false
+                }
+            ]
+        }
     }
 
 }).mount('#app')
