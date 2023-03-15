6a21888b9cc885f8f41ce740db6576cb84570bd1
danielebd <danielebalebalestro@gmail.com>
2023-03-15 16:28:53 +0100

    text bar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
+            activeCounter: 0,
             toDo: [
                 {
                     text: 'fare la spesa',
@@ -23,6 +24,26 @@ createApp({
                     done: false
                 }
             ]
+        }
+    },
+    methods: {
+        barClick(i){
+            if(this.toDo[i].done === false){
+                this.toDo[i].done = true;
+                return console.log(this.toDo[i].done)
+            }
+            else if(this.toDo[i].done === true){
+                this.toDo[i].done = false;
+                return console.log(this.toDo[i].done)
+            }
+        },
+        bar(i){
+            if(this.toDo[i].done === false){
+                return ''
+            }
+            else if (this.toDo[i].done === true){
+                return 'text-bar'
+            }
         }
     }
 
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ modificare lo style con css
 JS creare il collegamento con vue all'html e creare l'array con oggetto text(stringa da inniettare) e done(variabile booleana false di default).
 MILESTONE 1
 stampare ogni elemento della chiave text nel DOM tramite comando v-for così da dupplicare il li con il contenuto inserito
-crerare una classe in css textBar che andrà a modificare il testo barrandolo 
+creare una classe in css textBar che andrà a modificare il testo barrandolo 
 inserire nel li text un evento click con una funzione(clickBar) che andreamo a creare su js
 creare funzione clickBar in js che al click vada a modificare l'oggetto done in true se done is false e return e se is true return classeList.toogle('textBar')
 MILESTONE 2
